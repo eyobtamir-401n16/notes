bd3ec87ac99b35918192117f5852b0188ede28c1
Eyob1984 <tamireyob@gmail.com>
2020-03-18 15:06:07 -0700

    first phasetest start working

---
+++ _test_/input.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Input = require('../lib/input');
+
+// bad inputs 
+const badInput1 = [];
+const badInput2 = ['Wrong'];
+const badInput3 = ['-c', 'Wrong'];
+const badInput4 = ['-a', ''];
+
+// good inputs
+const goodInput = ['-a', 'This is a good note'];
+
+describe('bad input handler', () =>{
+  it('handle empty input', () =>{
+    
+    expect().toBe()
+  })
+  it('handle empty input', () =>{
+    
+    expect().toBe()
+  })
+  it('handle empty input', () =>{
+    
+    expect().toBe()
+  })
+  it('handle empty input', () =>{
+    
+    expect().toBe()
+  })
+})
+describe('good input handler',() =>{
+  it('handles good input', () => {
+    expect().toBe()
+  })
+})
+ 
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('Hello from the computer');
+
+console.log(process.args.splice());
