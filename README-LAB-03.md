d199b2e55334ef887c9cfc04552bbfd389fa8b53
Eyob1984 <tamireyob@gmail.com>
2020-03-27 17:11:30 -0700

    lab 03 README updated

---
+++ README-LAB-03.md
@@ -0,0 +1,30 @@
+# LAB - Class 03
+
+## Node Eco-system
+
+### Author: Eyob Tamir
+
+### Links and Resources
+
+- [PR Link](https://github.com/eyobtamir-401n16/notes/pull/3)
+
+
+### Setup
+
+
+#### How to initialize/run your application (where applicable)
+
+- e.g. run `node index.js -a "some text "`
+           `node index.js -l `
+           `node index.js -d `
+
+
+
+
+#### Tests
+
+- How do you run tests?   run   `npm test`.
+- Any tests of note?  Yes.
+- Describe any tests that you did not complete, skipped, etc. Its all completed
+
+#### UML
--- index.js
+++ index.js
@@ -18,37 +18,38 @@ mongoose.connect(dbURL, {
 });
 let args = process.argv.slice(2);
 let result = new Input(process.argv.slice(2));
+// console.log(result)
 let notes = new Notes(result);
-console.log(notes)
+// console.log(notes)
 const dbOpera = async () =>{
   let allNotes = await Mynotes.find();
   let allCategories = await Mycateogories.find();
   allNotes.forEach(value =>{
-    console.log(value.note)
+    // console.log(value.note)
   })
 
-  if(args.length > 0){
-    let newNote = new Mynotes({
-      note:args[0],
-    });
-    try{
-    let res = await newNote.save();
-    console.log(res);
-    } catch(error){
-      console.error(error);
-    }
-  }
+  // if(args.length > 0){
+  //   let newNote = new Mynotes({
+  //     note:args[0],
+  //   });
+  //   try{
+  //   let res = await newNote.save();
+  //   // console.log(res);
+  //   } catch(error){
+  //     console.error(error);
+  //   }
+  // }
   let allCats = await Mycateogories.findOne({name:'xyz'});
-  let catId = catId._id;
+
   
 
-console.log(allCats)
+// console.log(allCats)
 
   allCategories.forEach(value =>{
-    console.log(value.name);
+    // console.log(value.name);
   })
 
-  mongoose.disconnect()
+  
 }
 
 dbOpera();
